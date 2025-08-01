a4a2dac3a37e0bd113f998098d347b9356e17463
Mohamed Elkmeshi <M.Elkmeshi@prestoeat.com>
2025-08-01 20:26:23 +0200

    feat: Update remote URL in ConfigService to point to the correct config.json location

--- lib/services/config_service.dart
+++ lib/services/config_service.dart
@@ -3,11 +3,14 @@ import 'package:http/http.dart' as http;
 import '../models/category.dart';
 
 class ConfigService {
-  static const remoteUrl = 'https://raw.githubusercontent.com/<user>/<repo>/<branch>/assets/config.json';
+  static const remoteUrl =
+      'https://raw.githubusercontent.com/melkmeshi/flutter-sms/main/assets/config.json';
   Future<List<Category>> fetchCategories() async {
     final res = await http.get(Uri.parse(remoteUrl));
     if (res.statusCode != 200) throw Exception('Failed to load config');
     final map = json.decode(res.body) as Map<String, dynamic>;
-    return (map['categories'] as List).map((c) => Category.fromJson(c)).toList();
+    return (map['categories'] as List)
+        .map((c) => Category.fromJson(c))
+        .toList();
   }
 }
